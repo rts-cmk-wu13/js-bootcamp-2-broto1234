14041cffff679918621bb9092c22370f9440b5d8
Satyobroto <satyo97du@gmail.com>
2025-01-31 12:33:16 +0100

    Niveau2-02 Ekstraopgave

--- Opgaver/Niveau2/02/script.js
+++ Opgaver/Niveau2/02/script.js
@@ -6,20 +6,29 @@ products.forEach(function(product) {
   const productQuantity = product.querySelector(".product__quantity");
   const btnMinus = product.querySelector(".product__btn--minus");
   const btnPlus = product.querySelector(".product__btn--plus");
-  const btnPlus = product.querySelector(".product__btn--plus");
-  let quantity = 0;
+  const price = product.querySelector(".product__price");
+  const totalPrice = product.querySelector(".product__totalPrice");
+  console.log(price.textContent)
 
+  let quantity = 0;
+  
   btnPlus.addEventListener("click", function(){
     
     quantity++;
+    // console.log(quantity)
+    let tprice = price.textContent * quantity;
     productQuantity.textContent = quantity;
-
+    totalPrice.textContent = tprice;
+    
   })
-
+  
   btnMinus.addEventListener("click", function(){
+    // let tprice = Number(price) * quantity;
     if(quantity > 0)
       quantity--;
+    let tprice = price.textContent * quantity;
     productQuantity.textContent = quantity;
+    totalPrice.textContent = tprice;
   })
 })
 
